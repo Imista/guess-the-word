181963596dca7b5a3951f53d2ca95e2f1e04dc09
MuffinPlease <benjamintec3455@gmail.com>
2022-02-20 17:48:06 -0600

    New categories

--- src/data/categoryData.data.js
+++ src/data/categoryData.data.js
@@ -1,5 +1,5 @@
 const categoryData = () => {
-    return ['Frutas','Marcas','Colores','Red_social','Paises'];
+    return ['Frutas','Marcas','Colores','Red_social','Paises','Vehiculos','Deportes','Astronomia','Signo_zodiacal','Mamiferos'];
 }
 
 export default categoryData;
--- src/data/wordData.data.js
+++ src/data/wordData.data.js
@@ -4,8 +4,13 @@ const wordData = (category) => {
         Frutas: ['Limon','Fresa','Melon','Mango','Mora','Naranja','Pera','Platano','Sandia','Cereza','Uva','Lima','Manzana'],
         Marcas: ['Apple','Amazon','Microsoft','Google','Samsung','Cocacola','Disney','Netflix','Nike','Spotify','Paypal','Lego'],
         Colores: ['Azul','Rojo','Amarillo','Naranja','Rosa','Morado','Violeta','Verde','Negro','Blanco','Cafe'],
-        Red_social: ['Facebook','Youtube','Whatsapp','Instagram','Tiktok','Twitter','Telegram','Linkedin','Pinterest','Reddit','Cafe'],
-        Paises: ['Mexico','Canada','China','Brasil','Colombia','Argentina','Peru','Chile','Suecia','Japon','Dinamarca','Noruega','Suiza','Australia']
+        Red_social: ['Facebook','Youtube','Whatsapp','Instagram','Tiktok','Twitter','Telegram','Linkedin','Pinterest','Tinder'],
+        Paises: ['Mexico','Canada','China','Brasil','Colombia','Argentina','Peru','Chile','Suecia','Japon','Dinamarca','Noruega','Suiza','Australia'],
+        Vehiculos: ['Carro','Bicicleta','Motocicleta','Avion','Tren','Barco','Bote','Camion','Trailer','Tractor','Metro'],
+        Deportes: ['Futbol','Atletismo','Ciclismo','Natacion','Tenis','Voley','Basquetbol','Golf','Beisbol','Gimnasia','Patinaje'],
+        Astronomia: ['Mercurio','Venus','Tierra','Marte','Jupiter','Saturno','Urano','Neptuno','Pluton','Sol','Luna'],
+        Signo_zodiacal: ['Aries','Tauro','Geminis','Cancer','Leo','Virgo','Libra','Escorpion','Sagitario','Acuario','Piscis'],
+        Mamiferos: ['Ballena','Cabllo','Delfin','Elefante','Leon','Gato','Hipopotamo','Jirafa','Oso','Humano','Perro']
     }
     return (words[category]).map(word => word.toUpperCase());
 }
